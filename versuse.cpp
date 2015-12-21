c7569f7f294ff5c29e9683a901dbb2023bc52cbf
Ehwuts Matsu <tsukan.git@gmail.com>
2015-12-21 02:23:15 -0800

    trackbar visual

--- versuse.cpp
+++ versuse.cpp
@@ -1,8 +1,10 @@
 #include <windows.h>
+#include <commctrl.h>
+#include <cstdio>
 
 /*
 * this blob of defines makes me sad but defining things as local
-* constants broke more than I was up for fixing pre-function
+* constants broke more than I was up for fixing pre-functionality
 */
 #define VERSUSE_BUTTON_WRITE 101
 #define VERSUSE_EDIT_LEFTNAME 111
@@ -19,6 +21,8 @@
 #define VERSUSE_STATIC_L 131
 #define VERSUSE_STATIC_C 132
 #define VERSUSE_STATIC_R 133
+#define VERSUSE_STATIC_SLIDER 134
+#define VERSUSE_TRACKBAR_WIDTH 141
 #define VERSUSE_STRING_VERSION "v0.3"
 #define VERSUSE_STRING_L "L"
 #define VERSUSE_STRING_C "C"
@@ -26,6 +30,8 @@
 
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HWND widthRef;
+HWND trackRef;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -52,7 +58,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	HWND hWnd = CreateWindowEx(NULL, 
 	        wc.lpszClassName, "Versuse",
 			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, 200, 312, 124,
+			CW_USEDEFAULT, 200, 312, 144,
 			NULL, NULL,
 			hInstance, NULL);
 
@@ -148,42 +154,61 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 								GetModuleHandle(NULL), NULL);
 			init = CreateWindowEx(NULL, 
 								"STATIC", VERSUSE_STRING_VERSION, 
-								WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
-								184, 64, 36, 24,
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD,
+								188, 64, 36, 24,
 								hWnd, (HMENU)VERSUSE_STATIC_VERSION,
 								GetModuleHandle(NULL), NULL);
 			init = CreateWindowEx(NULL, 
 								"STATIC", VERSUSE_STRING_L, 
-								WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
-								226, 64, 24, 24,
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD,
+								234, 64, 24, 24,
 								hWnd, (HMENU)VERSUSE_STATIC_L,
 								GetModuleHandle(NULL), NULL);
 			init = CreateWindowEx(NULL, 
 								"STATIC", VERSUSE_STRING_C, 
-								WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
-								248, 64, 24, 24,
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD,
+								256, 64, 24, 24,
 								hWnd, (HMENU)VERSUSE_STATIC_C,
 								GetModuleHandle(NULL), NULL);
 			init = CreateWindowEx(NULL, 
 								"STATIC", VERSUSE_STRING_R, 
-								WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
-								270, 64, 24, 24,
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD,
+								278, 64, 24, 24,
 								hWnd, (HMENU)VERSUSE_STATIC_R,
 								GetModuleHandle(NULL), NULL);
+								
+								
+			trackRef = CreateWindowEx(NULL, 
+								"msctls_trackbar32", "Trackbar Control", 
+								WS_VISIBLE|WS_CHILD,
+								2, 88, 292, 12,
+								hWnd, (HMENU)VERSUSE_TRACKBAR_WIDTH,
+								GetModuleHandle(NULL), NULL);
+			widthRef = CreateWindowEx(NULL, 
+								"STATIC", "100", 
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD,
+								130, 64, 24, 24,
+								hWnd, (HMENU)VERSUSE_STATIC_SLIDER,
+								GetModuleHandle(NULL), NULL);
+		}
+		break;
+		case WM_HSCROLL:
+		{
+			LRESULT pos = SendMessage(trackRef, TBM_GETPOS, 0, 0);
+			char buf[4];
+			sprintf(buf, "%03ld", pos);
+			SetWindowText(widthRef, buf);
 		}
 		break;
 		case WM_COMMAND:
 			switch(LOWORD(wParam)) {
 				case VERSUSE_BUTTON_WRITE:
-					
+					MessageBox(hWnd, "This isn't implemented yet :(", "Hi", MB_OK);
 				break;
 				default:
 					acted = false;				
 			}
 		break;
-		/*case WM_RBUTTONDOWN:
-			MessageBox(hWnd, "v0.1", "Hi", MB_OK);
-		break;*/
         case WM_CLOSE:
             DestroyWindow(hWnd);
         break;
