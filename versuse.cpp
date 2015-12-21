f94f2bfe6050ce4152b72641656641509e8def06
Ehwuts Matsu <tsukan.git@gmail.com>
2015-12-21 04:47:01 -0800

    config loads are broken

--- versuse.cpp
+++ versuse.cpp
@@ -23,27 +23,30 @@
 #define VERSUSE_STATIC_R 133
 #define VERSUSE_STATIC_SLIDER 134
 #define VERSUSE_TRACKBAR_WIDTH 141
-#define VERSUSE_STRING_VERSION "v0.6"
+#define VERSUSE_STRING_VERSION "v0.7"
 #define VERSUSE_STRING_L "L"
 #define VERSUSE_STRING_C "C"
 #define VERSUSE_STRING_R "R"
-
+#define VERSUSE_STRING_CONFIG "versuse-conf.txt"
+#define VERSUSE_STRING_CONFIG_FORMAT "%s\n%d %d %d\n%s\n%s\n%s\n%s\nThings to avoid to prevent unexpected behaviour:\noutput filename length above 25 characters.\nname length above 40 characters\nscores more than 3 characters"
+#define VERSUSE_LIST_CONFIG_VARS_SET outfile, outw, alignL, alignR, leftname, leftscore, rightname, rightscore
+#define VERSUSE_LIST_CONFIG_VARS_GET &outfile, &outw, &alignL, &alignR, &leftname, &leftscore, &rightname, &rightscore
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void UpdateWidthDisplay(HWND hWnd);
 int SaveSettings();
 
-enum Align { LEFT = 1, CENTER = 2, RIGHT = 3 };
+//enum Align { LEFT = 1, CENTER = 2, RIGHT = 3 };
 
 // GLOBALS EVERYWHERE GLOBALS
-Align alignL = CENTER;
-Align alignR = CENTER;
-short outw = 68;
+int alignL = 2;
+int alignR = 2;
+int outw = 68;
 char leftname[41] = "";
 char rightname[41] = "";
-char outfile[26] = "versuse-conf.txt";
-short leftscore = 0;
-short rightscore = 0;
+char outfile[26] = "versuse-out.txt";
+char leftscore[4] = "0";
+char rightscore[4] = "0";
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {	
@@ -95,77 +98,83 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	switch(msg)	{
 		case WM_CREATE:
 		{
-			FILE *conf = fopen("versuse.conf", "r");
+			FILE *conf = fopen(VERSUSE_STRING_CONFIG, "r");
 			if (conf == NULL) {
 				//try write defaults
 				if (SaveSettings()) MessageBox(hWnd, "Failed to save default config.", "Err", MB_ICONEXCLAMATION | MB_OK);
 			} else {
 				//read in saved values
-				
+				fscanf(conf, VERSUSE_STRING_CONFIG_FORMAT, VERSUSE_LIST_CONFIG_VARS_GET);
 				fclose(conf);
 			}
-			
+			HWND init;
 			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", "", 
+							"EDIT", leftname, 
 							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
 							4, 4, 168, 24,
 							hWnd, (HMENU)VERSUSE_EDIT_LEFTNAME,
 							GetModuleHandle(NULL), NULL);
 			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", "", 
+							"EDIT", leftscore, 
 							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
 							184, 4, 36, 24,
 							hWnd, (HMENU)VERSUSE_EDIT_LEFTSCORE,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON|WS_GROUP,
 							232, 4, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_LEFT_L,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			if (alignL == 1) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
 							254, 4, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_LEFT_C,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			if (alignL == 2) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
 							276, 4, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_LEFT_R,
 							GetModuleHandle(NULL), NULL);
+			if (alignL == 3) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
 								
 			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", "", 
+							"EDIT", rightname, 
 							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
 							4, 32, 168, 24,
 							hWnd, (HMENU)VERSUSE_EDIT_RIGHTNAME,
 							GetModuleHandle(NULL), NULL);
 			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", "", 
+							"EDIT", rightscore, 
 							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
 							184, 32, 36, 24,
 							hWnd, (HMENU)VERSUSE_EDIT_RIGHTSCORE,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON|WS_GROUP,
 							232, 32, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_L,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			if (alignR == 1) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
 							254, 32, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_C,
 							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
+			if (alignR == 2) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
+			init = CreateWindowEx(NULL, 
 							"BUTTON", "", 
 							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
 							276, 32, 24, 24,
 							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_R,
 							GetModuleHandle(NULL), NULL);
+			if (alignR == 3) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
 								
 			CreateWindowEx(NULL, 
 							"BUTTON", "Write", 
@@ -225,6 +234,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		case WM_COMMAND:
 			switch(LOWORD(wParam)) {
 				case VERSUSE_BUTTON_WRITE:
+					//if (SaveSettings()) MessageBox(hWnd, "Failed to save current config.", "Err", MB_ICONEXCLAMATION | MB_OK);
 					MessageBox(hWnd, "This isn't implemented yet :(", "Hi", MB_OK);
 				break;
 				default:
@@ -262,9 +272,9 @@ std::string rightname = "";
 short rightscore = 0;
 */
 int SaveSettings() {
-	FILE *fd = fopen("versuse.conf", "w");
+	FILE *fd = fopen(VERSUSE_STRING_CONFIG, "w");
 	if (fd == NULL) return -1;
-	fprintf(fd, "%s\n%d %d %d\n%s\n%d\n%s\n%d\nMax supported output filename length is 25 characters.\nMax supported name length is 40 characters.", outfile, outw, alignL, alignR, leftname, leftscore, rightname, rightscore);
+	fprintf(fd, VERSUSE_STRING_CONFIG_FORMAT, VERSUSE_LIST_CONFIG_VARS_SET);
 	fclose(fd);
 	return 0;
 }
