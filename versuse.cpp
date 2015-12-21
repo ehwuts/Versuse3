ef67e70348c4fea19e9f4157840326843b233fd9
Ehwuts Matsu <tsukan.git@gmail.com>
2015-12-21 00:06:52 -0800

    text boxen

--- versuse.cpp
+++ versuse.cpp
@@ -1,5 +1,17 @@
 #include <windows.h>
 
+#define VERSUSE_BUTTON_WRITE 101
+#define VERSUSE_EDIT_LEFTNAME 111
+#define VERSUSE_EDIT_LEFTSCORE 113
+#define VERSUSE_EDIT_RIGHTNAME 112
+#define VERSUSE_EDIT_RIGHTSCORE 114
+#define VERSUSE_OPTION_LEFT_L 121
+#define VERSUSE_OPTION_LEFT_C 122
+#define VERSUSE_OPTION_LEFT_R 123
+#define VERSUSE_OPTION_RIGHT_L 124
+#define VERSUSE_OPTION_RIGHT_C 125
+#define VERSUSE_OPTION_RIGHT_R 126
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -27,7 +39,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	HWND hWnd = CreateWindowEx(NULL, 
 	        wc.lpszClassName, "Versuse",
 			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, 200, 640, 480,	NULL, NULL,
+			CW_USEDEFAULT, 200, 550, 411,
+			NULL, NULL,
 			hInstance, NULL);
 
 	if (hWnd == NULL) {
@@ -47,11 +60,55 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch(msg)
-	{
-		case WM_RBUTTONDOWN:
-			MessageBox(hWnd, "Hi", "How ya doin?", MB_OK | MB_ICONINFORMATION);
+	bool acted = true;
+	switch(msg)	{
+		case WM_CREATE:
+		{
+			HWND init;
+			init = CreateWindowEx(WS_EX_CLIENTEDGE, 
+								"Edit", "", 
+								WS_CHILD|WS_VISIBLE|ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
+								16, 4, 170, 24,
+								hWnd, (HMENU)VERSUSE_EDIT_LEFTNAME,
+								GetModuleHandle(NULL), NULL);
+			init = CreateWindowEx(WS_EX_CLIENTEDGE, 
+								"Edit", "", 
+								WS_CHILD|WS_VISIBLE|ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
+								190, 4, 36, 24,
+								hWnd, (HMENU)VERSUSE_EDIT_LEFTSCORE,
+								GetModuleHandle(NULL), NULL);
+			init = CreateWindowEx(WS_EX_CLIENTEDGE, 
+								"Edit", "", 
+								WS_CHILD|WS_VISIBLE|ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
+								16, 32, 170, 24,
+								hWnd, (HMENU)VERSUSE_EDIT_RIGHTNAME,
+								GetModuleHandle(NULL), NULL);
+			init = CreateWindowEx(WS_EX_CLIENTEDGE, 
+								"Edit", "", 
+								WS_CHILD|WS_VISIBLE|ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
+								190, 32, 36, 24,
+								hWnd, (HMENU)VERSUSE_EDIT_RIGHTSCORE,
+								GetModuleHandle(NULL), NULL);
+			init = CreateWindowEx(NULL, 
+								"Button", "Write", 
+								WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
+								4, 60, 100, 24,
+								hWnd, (HMENU)VERSUSE_BUTTON_WRITE,
+								GetModuleHandle(NULL), NULL);
+		}
 		break;
+		case WM_COMMAND:
+			switch(LOWORD(wParam)) {
+				case VERSUSE_BUTTON_WRITE:
+					
+				break;
+				default:
+					acted = false;				
+			}
+		break;
+		/*case WM_RBUTTONDOWN:
+			MessageBox(hWnd, "v0.1", "Hi", MB_OK);
+		break;*/
         case WM_CLOSE:
             DestroyWindow(hWnd);
         break;
@@ -59,8 +116,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			PostQuitMessage(0);
 		break;
 		default:
-			return DefWindowProc(hWnd,msg,wParam,lParam);
+			acted = false;
 	}
 
-	return 0;
+	if (acted) return 0;
+	else return DefWindowProc(hWnd,msg,wParam,lParam);
 }
