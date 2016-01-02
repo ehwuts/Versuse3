cdd975bdc31df5523197ee994627b906edae0739
Ehwuts Matsu <tsukan.git@gmail.com>
2016-01-01 18:43:21 -0800

    renamed stuff

--- makefile.sh
+++ makefile.sh
@@ -1,3 +1,3 @@
-windres ./src/versuse.rc -O coff -o ./src/versuse.res
-g++ -c -Wall ./src/versuse.cpp -o ./src/versuse.o
-g++ -o ./bin/Versuse.exe ./src/versuse.o ./src/versuse.res -mwindows
+windres ./src/versuse-resource.rc -O coff -o ./src/versuse-resource.res
+g++ -c -Wall ./src/versuse-main.cpp -o ./src/versuse-main.o
+g++ -o ./bin/Versuse.exe ./src/versuse-main.o ./src/versuse-resource.res -mwindows
