d66e4c533e4103bd816ba2277c58a884fcefc4e0
Ehwuts Matsu <tsukan.git@gmail.com>
2016-01-02 11:45:30 -0800

    converted to dialogness

--- src/versuse-main.cpp
+++ src/versuse-main.cpp
@@ -5,7 +5,7 @@
 
 #include "versuse-main.hpp"
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK DialogProc (HWND, UINT, WPARAM, LPARAM);
 void UpdateWidthDisplay(HWND hWnd);
 int ReadSettings();
 int SaveSettings();
@@ -27,56 +27,66 @@ char leftscore[4] = "0";
 char rightname[41] = "";
 char rightscore[4] = "0";
 
+HINSTANCE augh = 0;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {	
-    WNDCLASSEX wc = {0};
-		
-	wc.cbSize        = sizeof(wc);
-	wc.lpszClassName = "versuseMain";
-	wc.lpszMenuName  = NULL;
-	wc.lpfnWndProc   = (WNDPROC)WndProc;
-	wc.style         = CS_HREDRAW | CS_VREDRAW;
-	wc.hInstance     = hInstance;
-	wc.cbClsExtra    = NULL;
-	wc.cbWndExtra    = NULL;
-	wc.hIcon         = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(VERSUSE_ICON));
-	wc.hIconSm       = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(VERSUSE_ICON), IMAGE_ICON, 16, 16, 0);
-	wc.hCursor       = LoadCursor(NULL,IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-
-    if (!RegisterClassEx(&wc)) {
-        MessageBox(NULL, "Window Registration Failed!", "Error! aaaaaaa!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-	HWND hWnd = CreateWindowEx(NULL, 
-	        wc.lpszClassName, "Versuse",
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, 200, 312, 144,
-			NULL, NULL,
-			hInstance, NULL);
-
-	if (hWnd == NULL) {
+	augh = hInst;
+	HWND hDiag = 0;
+	
+	hDiag = CreateDialog(hInst, MAKEINTRESOURCE(TEST_MAIN), 0, (DLGPROC)DialogProc);
+	
+	if (hDiag == NULL) {
 		MessageBox(NULL, "Window Creation Failed!", "Error! aaaaaaa!", MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
 
-	ShowWindow(hWnd, nShowCmd);
-
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+        if (!IsDialogMessage(hDiag, &msg)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
 	}
-	return (int) msg.wParam;
+	return msg.wParam;
 }
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK DialogProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	bool acted = true;
+	switch (msg) {
+		case WM_INITDIALOG:
+		{
+			HICON hIcon = LoadIcon(augh, MAKEINTRESOURCE(TEST_ICON));
+			SendMessage(hWnd, WM_SETICON, WPARAM(TRUE), LPARAM(hIcon));
+			hIcon = LoadIcon(augh, MAKEINTRESOURCE(TEST_ICO2));
+			SendMessage(hWnd, WM_SETICON, WPARAM(FALSE), LPARAM(hIcon));
+		}
+		break;
+		case WM_COMMAND:
+		
+		break;
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+        break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+		break;
+		default:
+			acted = false;
+	}
+	return acted;
+}
+BOOL CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	bool acted = true;
 	switch(msg)	{
-		case WM_CREATE:
+		case WM_INITDIALOG:
 		{
+			HICON hIcon = LoadIcon(augh, MAKEINTRESOURCE(TEST_ICON));
+			SendMessage(hWnd, WM_SETICON, WPARAM(TRUE), LPARAM(hIcon));
+			hIcon = LoadIcon(augh, MAKEINTRESOURCE(TEST_ICO2));
+			SendMessage(hWnd, WM_SETICON, WPARAM(FALSE), LPARAM(hIcon));
+			
 			FILE *conf = fopen(VERSUSE_STRING_CONFIG, "r");
 			if (conf == NULL) {
 				//try write defaults
@@ -86,125 +96,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				fclose(conf);
 				if (ReadSettings()) MessageBox(hWnd, "Something went wrong reading existing config.", "Err", MB_ICONEXCLAMATION | MB_OK);
 			}
-			HWND init;
-			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", leftname, 
-							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
-							4, 4, 168, 24,
-							hWnd, (HMENU)VERSUSE_EDIT_LEFTNAME,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", leftscore, 
-							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
-							184, 4, 36, 24,
-							hWnd, (HMENU)VERSUSE_EDIT_LEFTSCORE,
-							GetModuleHandle(NULL), NULL);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON|WS_GROUP,
-							232, 4, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_LEFT_L,
-							GetModuleHandle(NULL), NULL);
-			if (alignL == 1) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
-							254, 4, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_LEFT_C,
-							GetModuleHandle(NULL), NULL);
-			if (alignL == 2) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
-							276, 4, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_LEFT_R,
-							GetModuleHandle(NULL), NULL);
-			if (alignL == 3) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-								
-			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", rightname, 
-							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
-							4, 32, 168, 24,
-							hWnd, (HMENU)VERSUSE_EDIT_RIGHTNAME,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(WS_EX_CLIENTEDGE, 
-							"EDIT", rightscore, 
-							WS_CHILD|WS_VISIBLE|ES_CENTER|ES_AUTOHSCROLL,
-							184, 32, 36, 24,
-							hWnd, (HMENU)VERSUSE_EDIT_RIGHTSCORE,
-							GetModuleHandle(NULL), NULL);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON|WS_GROUP,
-							232, 32, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_L,
-							GetModuleHandle(NULL), NULL);
-			if (alignR == 1) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
-							254, 32, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_C,
-							GetModuleHandle(NULL), NULL);
-			if (alignR == 2) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-			init = CreateWindowEx(NULL, 
-							"BUTTON", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_AUTORADIOBUTTON,
-							276, 32, 24, 24,
-							hWnd, (HMENU)VERSUSE_OPTION_RIGHT_R,
-							GetModuleHandle(NULL), NULL);
-			if (alignR == 3) SendMessage(init, BM_SETCHECK, BST_CHECKED, 0);
-								
-			CreateWindowEx(NULL, 
-							"BUTTON", "Write", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD|BS_DEFPUSHBUTTON,
-							4, 60, 100, 24,
-							hWnd, (HMENU)VERSUSE_BUTTON_WRITE,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
-							"STATIC", VERSUSE_STRING_VERSION, 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD,
-							188, 64, 36, 24,
-							hWnd, (HMENU)VERSUSE_STATIC_VERSION,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
-							"STATIC", VERSUSE_STRING_L, 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD,
-							234, 64, 24, 24,
-							hWnd, (HMENU)VERSUSE_STATIC_L,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
-							"STATIC", VERSUSE_STRING_C, 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD,
-							256, 64, 24, 24,
-							hWnd, (HMENU)VERSUSE_STATIC_C,
-							GetModuleHandle(NULL), NULL);
-			CreateWindowEx(NULL, 
-							"STATIC", VERSUSE_STRING_R, 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD,
-							278, 64, 24, 24,
-							hWnd, (HMENU)VERSUSE_STATIC_R,
-							GetModuleHandle(NULL), NULL);
-								
-								
-			CreateWindowEx(NULL, 
-							"STATIC", "", 
-							WS_TABSTOP|WS_VISIBLE|WS_CHILD,
-							130, 64, 24, 24,
-							hWnd, (HMENU)VERSUSE_STATIC_SLIDER,
-							GetModuleHandle(NULL), NULL);
-			init = CreateWindowEx(NULL, 
-							"msctls_trackbar32", "Output Width", 
-							WS_VISIBLE|WS_CHILD|TBS_AUTOTICKS|TBS_ENABLESELRANGE,
-							2, 88, 292, 12,
-							hWnd, (HMENU)VERSUSE_TRACKBAR_WIDTH,
-							GetModuleHandle(NULL), NULL);
-			SendMessage(init, TBM_SETRANGE, true, MAKELONG(51,161));
-			SendMessage(init, TBM_SETSEL, true, MAKELONG(68,100));
-			SendMessage(init, TBM_SETPAGESIZE, 0,  2); 
-			SendMessage(init, TBM_SETTICFREQ, 10, 0); 
-			SendMessage(init, TBM_SETPOS, true, outw); 
-			UpdateWidthDisplay(hWnd);
+			if (alignL == 1) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_LEFT_L), BM_SETCHECK, BST_CHECKED, 0);
+			if (alignL == 2) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_LEFT_C), BM_SETCHECK, BST_CHECKED, 0);
+			if (alignL == 3) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_LEFT_R), BM_SETCHECK, BST_CHECKED, 0);
+			if (alignR == 1) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_RIGHT_L), BM_SETCHECK, BST_CHECKED, 0);
+			if (alignR == 2) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_RIGHT_C), BM_SETCHECK, BST_CHECKED, 0);
+			if (alignR == 3) SendMessage(GetDlgItem(hWnd, VERSUSE_EDIT_RIGHT_R), BM_SETCHECK, BST_CHECKED, 0);
+			SetWindowText(GetDlgItem(hWnd, VERSUSE_EDIT_LEFTNAME), leftname.c_str());
+			SetWindowText(GetDlgItem(hWnd, VERSUSE_EDIT_LEFTSCORE), leftscore.c_str());
+			SetWindowText(GetDlgItem(hWnd, VERSUSE_EDIT_RIGHTNAME), rightname.c_str());
+			SetWindowText(GetDlgItem(hWnd, VERSUSE_EDIT_RIGHTSCORE), rightscore.c_str());
 		}
 		break;
 		case WM_HSCROLL:
--- src/versuse-main.hpp
+++ src/versuse-main.hpp
@@ -2,34 +2,30 @@
 #define VERSUSE_MAIN_H
 
 /*
-* this blob of defines makes me sad but defining things as local
-* constants broke more than I was up for fixing pre-functionality
+* in the land of defines everyone is sad
 */
 #define VERSUSE_STRING_VERSION "v1.1"
 #define VERSUSE_RES_VERSION 1,1,0,0
 #define VERSUSE_RES_VERSION2 "1.1"
 
+#define VERSUSE_MAIN 150
 #define VERSUSE_ICON 151
-#define VERSUSE_BUTTON_WRITE 101
+//#define VERSUSE_ICO2 152
+
 #define VERSUSE_EDIT_LEFTNAME 111
 #define VERSUSE_EDIT_LEFTSCORE 113
 #define VERSUSE_EDIT_RIGHTNAME 112
 #define VERSUSE_EDIT_RIGHTSCORE 114
+#define VERSUSE_BUTTON_WRITE 101
+
 #define VERSUSE_OPTION_LEFT_L 121
 #define VERSUSE_OPTION_LEFT_C 122
 #define VERSUSE_OPTION_LEFT_R 123
+
 #define VERSUSE_OPTION_RIGHT_L 124
 #define VERSUSE_OPTION_RIGHT_C 125
 #define VERSUSE_OPTION_RIGHT_R 126
-#define VERSUSE_STATIC_VERSION 131
-#define VERSUSE_STATIC_L 131
-#define VERSUSE_STATIC_C 132
-#define VERSUSE_STATIC_R 133
-#define VERSUSE_STATIC_SLIDER 134
-#define VERSUSE_TRACKBAR_WIDTH 141
-#define VERSUSE_STRING_L "L"
-#define VERSUSE_STRING_C "C"
-#define VERSUSE_STRING_R "R"
+
 #define VERSUSE_STRING_CONFIG "versuse-conf.txt"
 #define VERSUSE_STRING_CONFIG_FORMAT "%s\n%d %d %d %d\n%s\n%s\n%s\n%s\nThings to avoid to prevent unexpected behaviour:\noutput filename length above 25 characters.\nname length above 40 characters\nscores more than 3 characters"
 #define VERSUSE_LIST_CONFIG_VARS_SET outfile, outw, alignL, alignR, mono, leftname, leftscore, rightname, rightscore
